de18733348befbda48139c09d9ee35cc609aa2f2
Vlad <vladdikevich@mail.ru>
2022-10-25 13:23:00 +0300

    New wrile in console

--- Project 1/Program.cs
+++ Project 1/Program.cs
@@ -11,6 +11,8 @@ namespace Project_1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world!");
+            Console.WriteLine("Hello world!");
+            Console.WriteLine("Hello world!");
         }
     }
 }
